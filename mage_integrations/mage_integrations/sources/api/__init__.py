038d0b5d3484d2c9a881097dfd28cf8dd2469150
Luis <luis@pop-os.localdomain>
2023-05-24 23:17:27 -0300

    fixed api source discover method

--- mage_integrations/mage_integrations/sources/api/__init__.py
+++ mage_integrations/mage_integrations/sources/api/__init__.py
@@ -18,6 +18,7 @@ import pandas as pd
 import magic
 import polars
 from io import BytesIO, StringIO
+import types
 
 
 class Api(Source):
@@ -28,6 +29,63 @@ class Api(Source):
     def discover(self, streams: List[str] = None) -> Catalog:
         streams = []
 
+        if isinstance(type(self.load_data()), types.GeneratorType) is False:
+            stream_id = 'api'
+
+            df = self.load_data().to_pandas()
+
+            properties = {}
+            for col in df.columns:
+                df_filtered = df[df[col].notnull()][[col]]
+
+                for k, v in infer_dtypes(df_filtered).items():
+                    if 'mixed' == v:
+                        counter = Counter(map(type, df_filtered[col].values))
+                        dtype, count = sorted(
+                            [(k, v) for k, v in dict(counter).items()],
+                            key=lambda t: t[1],
+                            reverse=True,
+                        )[0]
+
+                        if dtype is list:
+                            col_type = COLUMN_TYPE_ARRAY
+                        elif dtype is dict:
+                            col_type = COLUMN_TYPE_OBJECT
+                        else:
+                            col_type = COLUMN_TYPE_STRING
+                    else:
+                        col_type = convert_data_type(v)
+                    properties[k] = dict(
+                        type=[
+                            'null',
+                            col_type,
+                        ],
+                    )
+
+            schema = Schema.from_dict(dict(
+                properties=properties,
+                type='object',
+            ))
+
+            metadata = get_standard_metadata(
+                key_properties=[],
+                replication_method=REPLICATION_METHOD_FULL_TABLE,
+                schema=schema.to_dict(),
+                stream_id=stream_id,
+            )
+            catalog_entry = CatalogEntry(
+                key_properties=[],
+                metadata=metadata,
+                replication_method=REPLICATION_METHOD_FULL_TABLE,
+                schema=schema,
+                stream=stream_id,
+                tap_stream_id=stream_id,
+                unique_conflict_method=UNIQUE_CONFLICT_METHOD_UPDATE,
+            )
+
+            streams.append(catalog_entry)
+
+            return Catalog(streams) 
         for rows in self.load_data():
             if len(rows) >= 1:
                 stream_id = 'api'
@@ -87,7 +145,7 @@ class Api(Source):
 
         return Catalog(streams)
 
-    def _deal_with_google_sheets(self, response, separator, header):
+    def __deal_with_google_sheets(self, response, separator, header):
         url = self.config['url']
 
         if url.endswith('output=csv'):
@@ -105,7 +163,7 @@ class Api(Source):
         else:
             raise Exception('Extension not allowed, please use CSV,TSV or XLSX')
 
-    def _check_response_type(self, response):
+    def __check_response_type(self, response):
         """This function checks the type of response for:
         CSV,TSV,Excel file or JSON
 
@@ -114,7 +172,6 @@ class Api(Source):
         """
 
         #TODO add google sheets support cuz probably the normal use case
-        #TODO probably add pandas or polars response to look nice
         #TODO add separator config to distinguish between CSV or TSV or specific (default to ,)
         #TODO add has_header param
 
@@ -207,10 +264,7 @@ class Api(Source):
         checked_type = self._check_response_type(response)
 
         if type(checked_type) != str:
-            pandas = self.config.get['pandas']
-            if pandas is True:
-                yield checked_type.to_pandas()
-            yield checked_type
+            return checked_type
 
         else:
             result = response.json()
